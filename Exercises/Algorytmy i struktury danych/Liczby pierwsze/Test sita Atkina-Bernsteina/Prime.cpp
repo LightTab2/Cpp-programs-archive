7925f14342dbc97a82ec22bdbb08f6fe7e74cacd
LightTab2 <azorro12@gmail.com>
2019-07-03 21:55:25 +0200

    "Test sita Atkina-Bernsteina" added, it's weird.

--- Exercises/Algorytmy i struktury danych/Liczby pierwsze/Test sita Atkina-Bernsteina/Prime.cpp
+++ Exercises/Algorytmy i struktury danych/Liczby pierwsze/Test sita Atkina-Bernsteina/Prime.cpp
@@ -21,11 +21,10 @@ int main(int argc, char *args[])
     if (!nolog) cout << "Tresc zadania, ktore ten program rozwiazuje to:\n"
         "Znajdz wszystkie liczby w przedziale <1;n>.\n"
         "Rozwiazanie:\n"
-        "Tworze i wypisuje sito liniowe, czyli strukture zawierajaca wylacznie liczby pierwsze w taki "
-        "sposob:\n\n"
-        "Algorytm zaczyna od 2 i oznaczmy te liczbe x\n"
-        "Wykluczam ze zbioru wszystkich liczb naturalnych z przedzialu <1;n> wielokrotnosci x:\n"
-        "Usuwam ze zbioru kazda liczbe w niej znajdujaca sie, mnozona przez x, az przekroczy n\n\n";
+        "Tworze i wypisuje sito Atkina-Bersteina, ktorego dzialanie nie rozumiem.\n"
+        "https://cr.yp.to/papers/primesieves.pdf tutaj znalazlem dowod matematyczny, co mi wcale nie pomoglo "
+        "jezeli jestes w stanie to rozczytac to nie wiem co tutaj robisz. Moze jak ogarne liczby zespolone "
+        "i teorie pierscieni to wroce napisac dowod";
 
     if (!nolog) cout << "Podaj liczbe naturalna n:" << endl;
     cin >> n;
@@ -37,29 +36,46 @@ int main(int argc, char *args[])
     if (timer) start = chrono::high_resolution_clock::now();
     primes = new bool[n];
 
-    for (unsigned long long x = 0; x != n; ++x) primes[x] = true;
-
     if (timer) stop = chrono::high_resolution_clock::now();
     
-    for (unsigned long long i = 3; i != n; ++i) primes[i] = true;
-    unsigned long long a = 2, b, c;
-    while (a * a < n) //a * a to najmniejsza wielokrotność rozpatrywana (mniejsze zostały już wykluczone przy poprzednich wartościach a)
+    for (unsigned long long i = 5; i != n; ++i) primes[false] = true;
+    
+    unsigned long long sq = sqrt(n-1) + 1;
+    //na podstawie tego chorego dowodu
+    for (unsigned long long x = 1; x != sq; ++x)
     {
-        b = a;
-        while ((c = a * b) < n)
+        unsigned long long xx = x * x;
+        for (unsigned long long y = 1; y != sq; ++y)
         {
-            while (c < n)
-            {
-                primes[c] = false;
-                c *= a;
-            }
-            while (!primes[++b]); //znajdujemy następny element w zbiorze dla b
-        }
-        while (!primes[++a]); //up dla a
-    }
-    cout << 1 << ' ' << 2 << ' ';
-    for (unsigned long long i = 3; i != n; ++i) if(primes[i]) cout << i << " ";
+            unsigned long long yy = y * y,
+            z = (xx << 2) + yy,
+            zmod = z % 12;
+            if (z < n && (zmod == 1 || zmod == 5)) primes[z] = !primes[z];
 
+            z -= xx;
+            if (z < n && z % 12 == 7) primes[z] = !primes[z];
+
+            if (x > y)
+            {
+                z -= yy << 1;
+                if (z <= n && z % 12 == 11) primes[z] = !primes[z];
+            } 
+        }
+    }
+    //wykluczamy liczby, ktore nie sa bezkwadratowe
+    for (unsigned long long i = 5; i != sq; ++i)
+        if (primes[i])
+        {
+            unsigned long long xx = i * i,
+            z = xx;
+            while (z <= n)
+            {
+                primes[z] = false;
+                z += xx;
+            }
+        }
+    cout << 1 << ' ' << 2 << ' ' << 3 << ' ';
+    for (unsigned long long i = 5; i != n; ++i) if(primes[i]) cout << i << ' ';
     cout << endl;
     if (timer)
     {
